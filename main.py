8faa644e085a8e51fd06b1d3dcea6b3ff44ded0b
samuu159 <100205384+samuu159@users.noreply.github.com>
2025-01-09 17:30:49 +0100

    Añado dos funciones nuevas para calcular la suma máxima tuentística(incompleto)

---
+++ main.py
@@ -0,0 +1,40 @@
+from num2words import num2words
+import sys
+
+def es_tuentistico(num):
+
+    return "twenty" in num2words(num)
+
+def sumaMaxTuentistica(n):
+
+    numerosTuentistico = [i for i in range(n + 1) if es_tuentistico(i)]
+
+    if not numerosTuentistico:
+        return "IMPOSSIBLE"
+
+    cont = 0
+    total = 0
+    for t_num in sorted(numerosTuentistico, reverse=True):
+            total += t_num
+            cont += 1
+            return cont
+
+    return "IMPOSSIBLE"
+
+
+def main():
+    input_lines = sys.stdin.read().strip().split("\n")
+    c = int(input_lines[0])
+
+    resultados = []
+
+    for case_number in range(1, c + 1):
+        n = int(input_lines[case_number])
+        resultado = sumaMaxTuentistica(n)
+        resultados.append(f"Case #{case_number}: {resultado}")
+
+    for resultado in resultados:
+        print(resultado)
+
+if __name__ == "_main_":
+   main()
