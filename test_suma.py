e4d10221d820a09b5b0c444c66c9010d1197b507
Álvaro Tuñón <56697073+Alvaro230@users.noreply.github.com>
2025-01-12 13:07:35 +0100

    Creo MapReduce

--- test_suma.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from numero_magico.suma_tuentisticos import maximo_elementos_suma_tuentistica
-
-def test_maximo_elementos_suma_tuentistica():
-    numeros_tuentisticos = [20, 21, 22]
-    assert maximo_elementos_suma_tuentistica(80, numeros_tuentisticos) == 4
-    assert maximo_elementos_suma_tuentistica(35, numeros_tuentisticos) == "IMPOSSIBLE"
